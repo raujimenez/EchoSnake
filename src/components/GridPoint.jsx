c810cf8799c3994d9f9dcb16503babe5771475d1
Raul Jimenez <officialrauljimenez@gmail.com>
2020-03-08 17:42:00 -0500

    format

--- src/components/GridPoint.jsx
+++ src/components/GridPoint.jsx
@@ -1,33 +1,32 @@
-import React, { useContext } from 'react';
-import GameInfoContext from '../context/GameInfoContext.jsx';
-
+import React, { useContext } from "react";
+import GameInfoContext from "../context/GameInfoContext.jsx";
 
 function GridPoint(props) {
-    const row = props.row;
-    const col = props.column;
-    const containsBody = props.body;
-    const containsFruit = props.fruit;
-    const portalDestination = props.portalDestination;
-    const { themeHook } = useContext(GameInfoContext);
+  const row = props.row;
+  const col = props.column;
+  const containsBody = props.body;
+  const containsFruit = props.fruit;
+  const portalDestination = props.portalDestination;
+  const { themeHook } = useContext(GameInfoContext);
 
-    const GridPointStyles = {
-        gridRow: row,
-        gridColumn: col,
-        padding: '10px',
-        backgroundColor: containsFruit ? 'red' :
-                         portalDestination ? 'orange':  
-                         containsBody ? 'green' :
-                         themeHook[0] === 'light' ? 'whitesmoke' :
-                         '#383838',
-        borderStyle: 'none',
-        borderWidth: '0.1px'
+  const GridPointStyles = {
+    gridRow: row,
+    gridColumn: col,
+    padding: "10px",
+    backgroundColor: containsFruit
+      ? "red"
+      : portalDestination
+      ? "orange"
+      : containsBody
+      ? "green"
+      : themeHook[0] === "light"
+      ? "whitesmoke"
+      : "#383838",
+    borderStyle: "none",
+    borderWidth: "0.1px"
+  };
 
-    }
-
-    return (
-        <div style={GridPointStyles}>
-        </div>
-    )
+  return <div style={GridPointStyles}></div>;
 }
 
 export default GridPoint;
